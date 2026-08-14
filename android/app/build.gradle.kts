10458009e943c1eca5a881126b4c08728298b393
yjkim9670 <148088384+yjkim9670@users.noreply.github.com>
2026-08-14 22:45:52 +0900

    Enable Android BuildConfig generation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,6 +15,10 @@ android {
         versionName = "1.0.0"
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false

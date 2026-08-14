8207e14dfc750e8c89286234a00e2da3177c3e8a
yjkim9670 <148088384+yjkim9670@users.noreply.github.com>
2026-08-14 22:41:57 +0900

    Add Android app module

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.yjkim9670.codexworkbench"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.yjkim9670.codexworkbench"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}

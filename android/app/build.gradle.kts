3d18e3838723e0a524c79eaee85d1183db4865ec
yjkim9670 <148088384+yjkim9670@users.noreply.github.com>
2026-08-15 23:59:19 +0900

    Bump Android client to 1.1.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yjkim9670.codexworkbench"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.2"
+        versionCode = 5
+        versionName = "1.1.3"
     }
 
     buildFeatures {
